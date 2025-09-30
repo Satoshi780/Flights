8bd0f09a3bd2b41f2bb387cca11d9653320e8677
Susparsh Jakhmola <susparshj@gmail.com>
2025-10-01 01:40:15 +0530

    Added getAll airplanes api

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -7,54 +7,33 @@ class CrudRepository {
 
     async create(data) {
             const response = await this.model.create(data);
-            return response;
-            
+            return response;  
     }
 
     async destroy(data) {
-        try {
-            const response = await this.model.destroy({
-                where: {
-                    id :data 
-                }
-            });
-            return response; // returns number of rows deleted
-        } catch (error) {
-            Logger.error('Something went wrong in the Crud Repo : destroy', error);
-            throw error;
-        }
+        const response = await this.model.destroy({
+            where: {
+                id :data 
+            }
+         });
+        return response; // returns number of rows deleted
     }
 
     async get(data) {
-        try {
             const response = await this.model.findByPk(data);
             return response;
-        } catch (error) {
-            Logger.error('Something went wrong in the Crud Repo : get', error);
-            throw error;
-        }
     }
     async getAll() {
-        try {
             const response = await this.model.findAll();
             return response;
-        } catch (error) {
-            Logger.error('Something went wrong in the Crud Repo : getAll', error);
-            throw error;
-        }
     }
     async update(id,data) {//data ->{col:value, ....}
-        try {
-            const response = await this.model.update(data,{
-                where:{
-                    id:id
-                }
+        const response = await this.model.update(data,{
+        where:{
+                id:id
+            }
             });
-            return response;
-        } catch (error) {
-            Logger.error('Something went wrong in the Crud Repo : update', error);
-            throw error;
-        }
+        return response;
     }
 }
 
--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -23,6 +23,16 @@ async function createAirplane(data){
     }
 }
 
+async function getAirplanes(){
+    try{
+        const airplanes= await airplaneRepository.getAll();
+        return airplanes;
+    }catch(error){
+        const appError = new AppError('Cannot fetch data of all airplanes',StatusCodes.INTERNAL_SERVER_ERROR);
+        throw appError; 
+    }
+}
 module.exports={
-    createAirplane
+    createAirplane,
+    getAirplanes
 }
