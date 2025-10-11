04ac18573a4a594172b85fa5fcccca86e363ab8c
Susparsh Jakhmola <susparshj@gmail.com>
2025-10-11 23:20:19 +0530

    Added new migration to add foreign key constraint in one to many relation
    using the following command
    '''
    npx sequelize migration:generate --name update-city-airport-association
    '''

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express=require('express');
 
 const {ServerConfig ,Logger}=require('./config');
 const apiRoutes=require('./routes');
+const city = require('./models/city');
 
 
 const app=express();
@@ -10,6 +11,29 @@ app.use(express.urlencoded({extended:true}));
 app.use('/api',apiRoutes);
 
 
-app.listen(ServerConfig.PORT,() => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
+    //bad code alert
+    const {City,Airport}=require('./models');
+    const bengaluru= await City.findByPk(2);
+    // const airport= await Airport.create({
+    //     name : 'Kempegowda International Airport',
+    //     code : 'BLR',
+    //     address : 'Devanahalli, Bengaluru, Karnataka, India',
+    //     cityId : bengaluru.id
+    // })
+    // const dbairport=await bengaluru.createAirport({
+    //     name : 'Hubbali Airport',
+    //     code : 'HBX',
+    //     address : 'Hubbali, Karnataka, India'
+    // });
+    // console.log(dbairport);
+    // console.log(bengaluru);
+    // const city=await City.findByPk(4);
+    // await city.createAirport({
+    //     name :'Indore Airport',
+    //     code : 'IDR',
+    //     address : 'Indore, Madhya Pradesh, India'
+    // })
+    // console.log(city);
 })
