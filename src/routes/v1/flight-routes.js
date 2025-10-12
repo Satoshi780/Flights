19c1bb5b45d7d507cec1f4d277fb62be372557cc
Susparsh Jakhmola <susparshj@gmail.com>
2025-10-12 23:21:55 +0530

    FEAT :added create flights api

---
+++ src/routes/v1/flight-routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const { FlightController } = require('../../controllers');
+
+const { FlightMiddlewares } = require('../../middlewares');
+const router = express.Router();
+
+// /api/v1/flights POST
+router.post('/', 
+    FlightMiddlewares.validateCreateRequest, 
+    FlightController.createFlight);
+
+// /api/v1/flights GET
+router.get('/', 
+    FlightController.getFlights);
+
+// /api/v1/flights/:id GET
+router.get('/:id', 
+    FlightController.getFlight);
+
+module.exports = router;
