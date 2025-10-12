ca108ee28673ddd2c2cc97599962c9d3831e2a2e
Susparsh Jakhmola <susparshj@gmail.com>
2025-10-12 18:56:29 +0530

    Added flights model with foreign key constraint defined during model definition npx sequelize model:generate --name Flight --attributes flightNumber:string,airplaneId:integer,departureAirportId:integer,arrivalAirportId:integer,arrival Time:date,departureTime:date,price:integer,boardingGate:string,totalSeats:integer

--- src/index.js
+++ src/index.js
@@ -13,28 +13,4 @@ app.use('/api',apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
-    //bad code alert
-    const {City,Airport}=require('./models');
-    const bengaluru= await City.findByPk(2);
-    // const airport= await Airport.create({
-    //     name : 'Kempegowda International Airport',
-    //     code : 'BLR',
-    //     address : 'Devanahalli, Bengaluru, Karnataka, India',
-    //     cityId : bengaluru.id
-    // })
-    // const dbairport=await bengaluru.createAirport({
-    //     name : 'Hubbali Airport',
-    //     code : 'HBX',
-    //     address : 'Hubbali, Karnataka, India'
-    // });
-    // console.log(dbairport);
-    // console.log(bengaluru);
-    // const city=await City.findByPk(4);
-    // await city.createAirport({
-    //     name :'Indore Airport',
-    //     code : 'IDR',
-    //     address : 'Indore, Madhya Pradesh, India'
-    // })
-    // console.log(city);
-    
 })
