455da928f899b8d6fb2bd3dab534e71d42cc17b7
Susparsh Jakhmola <susparshj@gmail.com>
2025-10-19 23:36:50 +0530

    Added seats model in our db associated to airplanes and some joins too added

--- src/utils/common/index.js
+++ src/utils/common/index.js
@@ -1,4 +1,9 @@
-module.exports={
+const enums = require('./enums');
+
+module.exports = {
     ErrorResponse: require('./error-response'),
-    SuccessResponse: require('./success-response')
+    SuccessResponse: require('./success-response'),
+    // provide both UPPERCASE and camelCase exports for compatibility
+    ENUMS: enums,
+    Enums: enums
 }
